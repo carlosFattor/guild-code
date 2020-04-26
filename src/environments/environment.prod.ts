9f7b6d89617d8df67b20ef6de4a942060535c9fb
Carlos <carlos.fattor@gmail.com>
2020-04-26 17:56:32 -0300

    change URI to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   clientId: '7c88c94314b715f98752',
   clientSecret: '23d2d25acc0e48d47fe40aca3c20c99df0c703e9',
-  server: 'http://localhost:3000'
+  server: 'guild-code-back.netlify.app'
 };
