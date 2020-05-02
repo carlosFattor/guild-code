8f974737f39698de5c5736d8180440c2fda46b2b
Carlos <carlos.fattor@gmail.com>
2020-05-02 17:50:39 -0300

    heroku config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   clientId: '7c88c94314b715f98752',
   clientSecret: '23d2d25acc0e48d47fe40aca3c20c99df0c703e9',
-  server: 'http://localhost:3000',
+  server: 'https://guild-code-back.herokuapp.com/',
   VAPID_PUBLIC_KEY: 'BIsoIvPiDLfPD3fHDl2ezTAiE3uYfq9n61lcIFkjY0PLdWboiTWAHdnTDZ3tGbcvmkopcsYFB2c57rBSpYhp9Fs'
 };
